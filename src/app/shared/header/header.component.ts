44d88d1bfab58d1d693a4d5f0bf34844629d6fd9
Carina <carinavbritto@gmail.com>
2024-08-05 12:17:30 -0300

    feat: responsive changes 2

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
 import { NgOptimizedImage } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [NavbarComponent, NgOptimizedImage],
+  imports: [NavbarComponent, NgOptimizedImage, RouterLink],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
